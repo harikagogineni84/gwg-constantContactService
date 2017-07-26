25a7a88bf3e20bc3e056b0a17ed1faca1d8e82bc
trevorbennett <garbage@trevorbennett.us>
2017-07-25 21:32:32 -0400

    renaming and simplification of return object on exception + gitignore changes

--- src/main/java/com/gwg/constantContact/signup/restService/RestServiceError.java
+++ src/main/java/com/gwg/constantContact/signup/restService/RestServiceError.java
@@ -2,21 +2,15 @@ package com.gwg.constantContact.signup.restService;
 
 public class RestServiceError {
 	
-	public String masheryMessageKey;
-	public String masheryMessageText;
+	public String masheryMessage;
 	public String errorText;
 	
-	public String getMasheryMessageKey() {
-		return masheryMessageKey;
+
+	public String getMasheryMessage() {
+		return masheryMessage;
 	}
-	public void setMasheryMessageKey(String masheryMessageKey) {
-		this.masheryMessageKey = masheryMessageKey;
-	}
-	public String getMasheryMessageText() {
-		return masheryMessageText;
-	}
-	public void setMasheryMessageText(String masheryMessageText) {
-		this.masheryMessageText = masheryMessageText;
+	public void setMasheryMessage(String masheryMessage) {
+		this.masheryMessage = masheryMessage;
 	}
 	public String getErrorText() {
 		return errorText;
--- src/main/java/com/gwg/constantContact/signup/restService/RestServiceErrorHandler.java
+++ src/main/java/com/gwg/constantContact/signup/restService/RestServiceErrorHandler.java
@@ -2,7 +2,6 @@ package com.gwg.constantContact.signup.restService;
 
 import org.springframework.http.HttpStatus;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RestServiceErrorHandler {
@@ -14,23 +13,23 @@ public class RestServiceErrorHandler {
 		RestServiceError error = retrieveMasheryException(exception);
 		error.setErrorText("System Error Occurred.");
 		
-		if(error.getMasheryMessageText().contains(""+ HttpStatus.NOT_FOUND.value())) {
+		if(error.getMasheryMessage().contains(""+ HttpStatus.NOT_FOUND.value())) {
 			error.setErrorText("An account with the requested Parameters could not be found.");
 		}
 		
-		if(error.getMasheryMessageText().contains(""+ HttpStatus.FORBIDDEN.value())) {
+		if(error.getMasheryMessage().contains(""+ HttpStatus.FORBIDDEN.value())) {
 			error.setErrorText("Permission denied. Sufficient access is required to access this constant contact account.");
 		}
 		
-		if(error.getMasheryMessageText().contains(""+ HttpStatus.SERVICE_UNAVAILABLE.value())) {
+		if(error.getMasheryMessage().contains(""+ HttpStatus.SERVICE_UNAVAILABLE.value())) {
 			error.setErrorText("The bulk contact push is temporarily unavailable.");
 		}
 		
-		if(error.getMasheryMessageText().contains(""+ HttpStatus.GATEWAY_TIMEOUT.value())) {
+		if(error.getMasheryMessage().contains(""+ HttpStatus.GATEWAY_TIMEOUT.value())) {
 			error.setErrorText("The connection to this service has timed out.");
 		}
 
-		if(error.getMasheryMessageText().contains(""+ HttpStatus.INTERNAL_SERVER_ERROR.value())) {
+		if(error.getMasheryMessage().contains(""+ HttpStatus.INTERNAL_SERVER_ERROR.value())) {
 			error.setErrorText("System Error Occurred.");
 		}
 		
@@ -39,7 +38,7 @@ public class RestServiceErrorHandler {
 
 	private RestServiceError retrieveMasheryException(Exception exception) {
 	RestServiceError error = new RestServiceError();
-	error.setMasheryMessageText(exception.getMessage());
+	error.setMasheryMessage(exception.getMessage());
 		return error;
 	}
 
