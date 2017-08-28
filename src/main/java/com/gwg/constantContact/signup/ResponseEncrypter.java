1159d2bc912722ee6541a1a20328c994f4f293a4
trevorbennett <garbage@trevorbennett.us>
2017-08-28 07:47:15 -0400

    response encryption using RSA if boolean flag is set in global.properties

--- src/main/java/com/gwg/constantContact/signup/ResponseEncrypter.java
+++ src/main/java/com/gwg/constantContact/signup/ResponseEncrypter.java
@@ -1,23 +1,30 @@
 package com.gwg.constantContact.signup;
 
+import java.security.Key;
+import java.util.Base64;
+
 import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class ResponseEncrypter {
 
+	
+	public static final String ALGORITHM = "RSA";
+	
 	public static String encrypt(String response, String privateKey) {
 		
 	    byte[] cipherText = null;
+	    byte[] decodedKey = Base64.getDecoder().decode(privateKey);
+	    Key key = new SecretKeySpec(decodedKey,0,decodedKey.length, "RSA");
 	    try {
-	      // get an RSA cipher object and print the provider
 	      final Cipher cipher = Cipher.getInstance(ALGORITHM);
-	      // encrypt the plain text using the public key
 	      cipher.init(Cipher.ENCRYPT_MODE, key);
-	      cipherText = cipher.doFinal(text.getBytes());
+	      cipherText = cipher.doFinal(response.getBytes());
 	    } catch (Exception e) {
 	      e.printStackTrace();
 	    }
 
-	    
+	    response = cipherText.toString();
 		return response;
 	}
 
